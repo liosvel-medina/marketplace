e326ef323f0e6703566fdf3de93d656942fbe147
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2023-09-27 17:18:04 -0400

    define font-family and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'black': '#000000',
+      'white': '#ffffff',
+      'dark': '#1C1C1C',
+      'gray-600': '#505050',
+      'gray-500': '#8B96A5',
+      'gray-400': '#BDC4CD',
+      'gray-300': '#DEE2E7',
+      'gray-200': '#EFF2F4',
+      'gray-100': '#F7FAFC',
+      'base-color-blue-100': '#E3F0FF',
+      'primary': '#0D6EFD',
+      'light-green': '#C3FFCB',
+      'green': '#00B517',
+      'light-orange': '#FFE5BF',
+      'orange': '#FF9017',
+      'red': '#FA3434'
+    },
+    extend: {
+      fontFamily: {
+        'sans': 'Inter',
+      }
+    },
   },
   plugins: [],
 }
