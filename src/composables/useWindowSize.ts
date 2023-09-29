16619affda17744d11c7a8f7fb6b6e4bf9f66cdf
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2023-09-29 09:59:10 -0400

    range screen sizes in useWindowSize

--- src/composables/useWindowSize.ts
+++ src/composables/useWindowSize.ts
@@ -1,11 +1,35 @@
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted, computed } from 'vue'
 
 export const useWindowSize = () => {
-    const width = ref<number>()
+    const width = ref<number>(0)
 
     const handler = () => {
         width.value = window.innerWidth
     }
+
+    const xs = computed(() => {
+        return width.value > 0 && width.value < 640
+    })
+
+    const sm = computed(() => {
+        return width.value >= 640 && width.value < 768
+    })
+
+    const md = computed(() => {
+        return width.value >= 768 && width.value < 1024
+    })
+
+    const lg = computed(() => {
+        return width.value >= 1024 && width.value < 1280
+    })
+
+    const xl = computed(() => {
+        return width.value >= 1280 && width.value < 1536
+    })
+
+    const xxl = computed(() => {
+        return width.value >= 1536
+    })
 
     onMounted(() => {
         handler()
@@ -16,5 +40,5 @@ export const useWindowSize = () => {
         window.addEventListener('resize', handler)
     })
 
-    return { width }
+    return { width, xs, sm, md, lg, xl, xxl }
 }
