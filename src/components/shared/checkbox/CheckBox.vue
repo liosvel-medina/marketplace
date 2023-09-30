e9ffb23efe03c428a70707546eb36110cfdc5e52
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2023-09-30 01:49:47 -0400

    create CheckBox component

---
+++ src/components/shared/checkbox/CheckBox.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface Props {
+  name: string;
+  fieldId: string;
+  label?: string;
+  isChecked?: boolean;
+}
+
+const { isChecked } = defineProps<Props>();
+const emit = defineEmits(["change"]);
+
+const isCheckedModel = ref(isChecked == true);
+
+const onChange = () => {
+  emit("change", isCheckedModel.value);
+};
+</script>
+
+<template>
+  <label class="flex items-center cursor-pointer">
+    <input
+      type="checkbox"
+      :name="name"
+      :id="fieldId"
+      class="hidden"
+      :checked="isChecked == true"
+      @change="onChange()"
+      v-model="isCheckedModel"
+    />
+
+    <img
+      src="../../../assets/images/controls/checkbox-checked.svg"
+      alt=""
+      v-if="isCheckedModel"
+    />
+
+    <img
+      src="../../../assets/images/controls/checkbox-unchecked.svg"
+      alt=""
+      v-else
+    />
+
+    <span v-if="label">{{ label }}</span>
+  </label>
+</template>
+
+<style scoped lang="scss"></style>
