40604c3ca8992fc7e79e18270c7fb00d68a8dbb8
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2023-10-02 16:27:53 -0400

    add You may like products to PDD

--- src/views/products/details/ProductDetailsDesktop.vue
+++ src/views/products/details/ProductDetailsDesktop.vue
@@ -3,9 +3,17 @@ import { useProductDetails } from "./useProductDetails";
 import Loader from "../../../components/shared/loader/Loader.vue";
 import MainInfoCard from "./desktop/MainInfoCard.vue";
 import SecondaryInfoCard from "./desktop/SecondaryInfoCard.vue";
+import Card from "../../../components/shared/card/Card.vue";
 
-const { product, images, selectedImage, favImage, toggleFavorite, isLoading } =
-  useProductDetails();
+const {
+  product,
+  images,
+  selectedImage,
+  favImage,
+  toggleFavorite,
+  isLoading,
+  similarProducts,
+} = useProductDetails();
 </script>
 
 <template>
@@ -26,8 +34,43 @@ const { product, images, selectedImage, favImage, toggleFavorite, isLoading } =
         @toggle-favorite="toggleFavorite"
       />
 
-      <div class="grid grid-cols-[1fr_280px] mt-5">
+      <div class="grid grid-cols-[1fr_280px] gap-5 mt-5">
         <SecondaryInfoCard />
+
+        <Card class="self-start">
+          <div class="flex flex-col gap-[14px] pt-5 px-4 pb-9">
+            <h3 class="title-h6">You may like</h3>
+
+            <div class="flex flex-col gap-4">
+              <RouterLink
+                v-for="item of similarProducts.filter((_, index) => index < 5)"
+                :key="`similar${item.id}`"
+                :to="{ name: 'ProductDetails', params: { id: item.id } }"
+                class="grid grid-cols-[80px_1fr] items-center gap-[11px] rounded-md cursor-pointer hover:bg-gray-100"
+              >
+                <Card>
+                  <div class="w-20 min-w-20 h-20 p-[5px]">
+                    <img
+                      :src="item.image"
+                      alt="preview"
+                      class="w-full h-full object-contain"
+                    />
+                  </div>
+                </Card>
+
+                <div class="flex flex-col gap-[3px]">
+                  <span class="h-12 overflow-y-hidden">
+                    {{ item.title }}
+                  </span>
+
+                  <span class="text-gray-500"
+                    >${{ item.price.toFixed(2) }}</span
+                  >
+                </div>
+              </RouterLink>
+            </div>
+          </div>
+        </Card>
       </div>
     </div>
   </div>
